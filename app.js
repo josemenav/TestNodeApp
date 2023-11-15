1f94fec5372aa575b02930ebf78ba6f6e999f31e
José Antonio Mendoza Navarro <joseantoniomendozanavarro@gmail.com>
2023-11-15 07:38:59 -0600

    deleted moment library

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const moment = require("moment"); 
-let i = 5; 
+ let i = 5; 
 let j = 10; 
 console.log('Iniciando');
 console.log("La suma es " + (5+10));
 
-let fecha = moment().format("LL");
+let fecha = (5+10);
 console.log("Y la fecha es: ", fecha)
